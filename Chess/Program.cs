4faaa07ac5ca7fb9f8380458ade6b062bd6eec0c
aburlaka2 <aburlaka1@gmail.com>
2020-12-12 18:47:31 +0200

    Update Program.cs

--- Chess/Program.cs
+++ Chess/Program.cs
@@ -26,7 +26,6 @@ namespace Chess
                     {
                         char vertical = StartPosition[0];
                         char horizontal = StartPosition[1];
-                        //Console.Write(horizontal);
                         switch (vertical)
                         {
                             case 'a':
@@ -114,8 +113,6 @@ namespace Chess
                     }
                 }
 
-                Console.WriteLine(EnterStartPosX);
-                Console.WriteLine(EnterStartPosY);
                 if (Field[EnterStartPosX, EnterStartPosY].CellFigure != null)
                 {
                     bool newcell = false;
@@ -209,9 +206,13 @@ namespace Chess
                                     return;
                             }
                         }
-                       
+                        else
+                        {
+                            Console.WriteLine("Input is not on field");
+                        }
+
                     }
-                    Field[EnterNewPosX, EnterNewPosY].CellFigure.Move(Field, EnterNewPosX, EnterNewPosY, EnterStartPosX, EnterStartPosY);
+                    Field[EnterStartPosX, EnterStartPosY].CellFigure.Move(Field, EnterNewPosX, EnterNewPosY, EnterStartPosX, EnterStartPosY);
                 }
                 else
                 {
@@ -268,94 +269,94 @@ namespace Chess
         }
         static void StartPlacement(Cell[,] Field)
         {
-            King WhiteKing = new King(1,'K', 7, 4 );
+            King WhiteKing = new King(1, '♚', 7, 4 );
             WhiteKing.SetToCell(Field, 7, 4);
             WhiteKing.SetFigureColor(Field, 7, 4);
-            King BlackKing = new King(0,'K', 0, 4);
+            King BlackKing = new King(0, '♚', 0, 4);
             BlackKing.SetToCell(Field, 0, 4);
             BlackKing.SetFigureColor(Field, 0, 4);
-            Quinn WhiteQuinn = new Quinn(1,'Q', 7, 3);
+            Quinn WhiteQuinn = new Quinn(1, '♛', 7, 3);
             WhiteQuinn.SetToCell(Field, 7, 3);
             WhiteQuinn.SetFigureColor(Field, 7, 3);
-            Quinn BlackQuinn = new Quinn(0,'Q', 0, 3);
+            Quinn BlackQuinn = new Quinn(0, '♛', 0, 3);
             BlackQuinn.SetToCell(Field, 0, 3);
             BlackQuinn.SetFigureColor(Field, 0, 3);
-            Bishop WhiteBishop1 = new Bishop(1,'B', 7, 2);
+            Bishop WhiteBishop1 = new Bishop(1, '♝', 7, 2);
             WhiteBishop1.SetToCell(Field, 7, 2);
             WhiteBishop1.SetFigureColor(Field, 7, 2);
-            Bishop WhiteBishop2 = new Bishop(1,'B', 7, 5);
+            Bishop WhiteBishop2 = new Bishop(1, '♝', 7, 5);
             WhiteBishop2.SetToCell(Field, 7, 5);
             WhiteBishop2.SetFigureColor(Field, 7, 5);
-            Bishop BlackBishop1 = new Bishop(0,'B', 0, 2);
+            Bishop BlackBishop1 = new Bishop(0, '♝', 0, 2);
             BlackBishop1.SetToCell(Field, 0, 2);
             BlackBishop1.SetFigureColor(Field, 0, 2);
-            Bishop BlackBishop2 = new Bishop(0,'B', 0, 5);
+            Bishop BlackBishop2 = new Bishop(0, '♝', 0, 5);
             BlackBishop2.SetToCell(Field, 0, 5);
             BlackBishop2.SetFigureColor(Field, 0, 5);
-            Knight WhiteKnight1 = new Knight(1,'k', 7, 1);
+            Knight WhiteKnight1 = new Knight(1, '♞', 7, 1);
             WhiteKnight1.SetToCell(Field, 7, 1);
             WhiteKnight1.SetFigureColor(Field, 7, 1);
-            Knight WhiteKnight2 = new Knight(1,'k', 7, 6);
+            Knight WhiteKnight2 = new Knight(1, '♞', 7, 6);
             WhiteKnight2.SetToCell(Field, 7, 6);
             WhiteKnight2.SetFigureColor(Field, 7, 6);
-            Knight BlackKnight1 = new Knight(0,'k', 0, 1);
+            Knight BlackKnight1 = new Knight(0, '♞', 0, 1);
             BlackKnight1.SetToCell(Field, 0, 1);
             BlackKnight1.SetFigureColor(Field, 0, 1);
-            Knight BlackKnight2 = new Knight(0,'k', 0, 6);
+            Knight BlackKnight2 = new Knight(0, '♞', 0, 6);
             BlackKnight2.SetToCell(Field, 0, 6);
             BlackKnight2.SetFigureColor(Field, 0, 6);
-            Rook WhiteRook1 = new Rook(1,'R', 7, 0);
+            Rook WhiteRook1 = new Rook(1, '♜', 7, 0);
             WhiteRook1.SetToCell(Field, 7, 0);
             WhiteRook1.SetFigureColor(Field, 7, 0);
-            Rook WhiteRook2 = new Rook(1,'R', 7, 7);
+            Rook WhiteRook2 = new Rook(1, '♜', 7, 7);
             WhiteRook2.SetToCell(Field, 7, 7);
             WhiteRook2.SetFigureColor(Field, 7, 7);
-            Rook BlackRook1 = new Rook(0,'R', 0, 0);
+            Rook BlackRook1 = new Rook(0, '♜', 0, 0);
             BlackRook1.SetToCell(Field, 0, 0);
             BlackRook1.SetFigureColor(Field, 0, 0);
-            Rook BlackRook2 = new Rook(0,'R', 0, 7);
+            Rook BlackRook2 = new Rook(0, '♜', 0, 7);
             BlackRook2.SetToCell(Field, 0, 7);
             BlackRook2.SetFigureColor(Field, 0, 7);
-            Pawn WhitePawn1 = new Pawn(1,'i', 6, 0);
+            Pawn WhitePawn1 = new Pawn(1, '♟', 6, 0);
             WhitePawn1.SetToCell(Field, 6, 0);
             WhitePawn1.SetFigureColor(Field, 6, 0);
-            Pawn WhitePawn2 = new Pawn(1,'i', 6, 1);
+            Pawn WhitePawn2 = new Pawn(1, '♟', 6, 1);
             WhitePawn2.SetToCell(Field, 6, 1);
             WhitePawn2.SetFigureColor(Field, 6, 1);
-            Pawn WhitePawn3 = new Pawn(1,'i', 6, 2);
+            Pawn WhitePawn3 = new Pawn(1, '♟', 6, 2);
             WhitePawn3.SetToCell(Field, 6, 2);
             WhitePawn3.SetFigureColor(Field, 6, 2);
-            Pawn WhitePawn4 = new Pawn(1,'i', 6, 3);
+            Pawn WhitePawn4 = new Pawn(1, '♟', 6, 3);
             WhitePawn4.SetToCell(Field, 6, 3);
             WhitePawn4.SetFigureColor(Field, 6, 3);
-            Pawn WhitePawn5 = new Pawn(1,'i', 6, 4);
+            Pawn WhitePawn5 = new Pawn(1, '♟', 6, 4);
             WhitePawn5.SetToCell(Field, 6, 4);
             WhitePawn5.SetFigureColor(Field, 6, 4);
-            Pawn WhitePawn6 = new Pawn(1,'i', 6, 5);
+            Pawn WhitePawn6 = new Pawn(1, '♟', 6, 5);
             WhitePawn6.SetToCell(Field, 6, 5);
             WhitePawn6.SetFigureColor(Field, 6, 5);
-            Pawn WhitePawn7 = new Pawn(1,'i', 6, 6);
+            Pawn WhitePawn7 = new Pawn(1, '♟', 6, 6);
             WhitePawn7.SetToCell(Field, 6, 6);
             WhitePawn7.SetFigureColor(Field, 6, 6);
-            Pawn WhitePawn8 = new Pawn(1,'i', 6, 7);
+            Pawn WhitePawn8 = new Pawn(1, '♟', 6, 7);
             WhitePawn8.SetToCell(Field, 6, 7);
             WhitePawn8.SetFigureColor(Field, 6, 7);
-            Pawn BlackPawn1 = new Pawn(0,'i', 1, 0);
+            Pawn BlackPawn1 = new Pawn(0, '♟', 1, 0);
             BlackPawn1.SetToCell(Field, 1, 0);
             BlackPawn1.SetFigureColor(Field, 1, 0);
-            Pawn BlackPawn2 = new Pawn(0,'i', 1, 1);
+            Pawn BlackPawn2 = new Pawn(0, '♟', 1, 1);
             BlackPawn2.SetToCell(Field, 1, 1);
-            Pawn BlackPawn3 = new Pawn(0,'i', 1, 2);
+            Pawn BlackPawn3 = new Pawn(0, '♟', 1, 2);
             BlackPawn3.SetToCell(Field, 1, 2);
-            Pawn BlackPawn4 = new Pawn(0,'i', 1, 3);
+            Pawn BlackPawn4 = new Pawn(0, '♟', 1, 3);
             BlackPawn4.SetToCell(Field, 1, 3);
-            Pawn BlackPawn5 = new Pawn(0,'i', 1, 4);
+            Pawn BlackPawn5 = new Pawn(0, '♟', 1, 4);
             BlackPawn5.SetToCell(Field, 1, 4);
-            Pawn BlackPawn6 = new Pawn(0,'i', 1, 5);
+            Pawn BlackPawn6 = new Pawn(0, '♟', 1, 5);
             BlackPawn6.SetToCell(Field, 1, 5);
-            Pawn BlackPawn7 = new Pawn(0,'i', 1, 6);
+            Pawn BlackPawn7 = new Pawn(0, '♟', 1, 6);
             BlackPawn7.SetToCell(Field, 1, 6);
-            Pawn BlackPawn8 = new Pawn(0,'i', 1, 7);
+            Pawn BlackPawn8 = new Pawn(0, '♟', 1, 7);
             BlackPawn8.SetToCell(Field, 1, 7);
 
         }
